b992c680c55d2bd3eef6c79b1818e58e7706b85f
timmyproofficial <timmyshow96@gmail.com>
2024-07-29 10:05:01 +0100

    Remove unwanted code, files, and folders.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersModule } from './users/users.module';
-import { AgentsModule } from './agents/agents.module';
-import { MarketsModule } from './markets/markets.module';
-import { ErrandsModule } from './errands/errands.module';
-import { ItemsModule } from './items/items.module';
 import { GenresModule } from './genres/genres.module';
 import { AlbumsModule } from './albums/albums.module';
 import { MediaModule } from './media/media.module';
@@ -14,11 +9,6 @@ import { MediaModule } from './media/media.module';
     MongooseModule.forRoot(
       'mongodb+srv://timmyshow96:lBQgrgUGPEUAfSBw@cluster0.fch9y4r.mongodb.net/tosinBeeDb?retryWrites=true&w=majority&appName=Cluster0',
     ),
-    UsersModule,
-    AgentsModule,
-    MarketsModule,
-    ErrandsModule,
-    ItemsModule,
     GenresModule,
     AlbumsModule,
     MediaModule,
