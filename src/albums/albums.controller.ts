50d06f840d2644a013ba69301f74ec4d283a7fdd
timmyproofficial <timmyshow96@gmail.com>
2024-08-03 02:38:03 +0100

    Setup SwaggerUI and Document Album Endpoint

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -14,12 +14,76 @@ import mongoose from 'mongoose';
 import { AlbumsService } from './albums.service';
 import { CreateAlbumDto } from './dto/Album.dto';
 import { UpdateAlbumDto } from './dto/UpdateAlbum.dto';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Albums')
 @Controller('albums')
 export class AlbumsController {
   constructor(private albumsService: AlbumsService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Fetch Albums' })
+  @ApiResponse({
+    status: 200,
+    description: 'Ok',
+    schema: {
+      type: 'object',
+      properties: {
+        count: {
+          type: 'number',
+          description: 'The length of the result',
+          example: 4,
+        },
+        result: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              _id: {
+                type: 'string',
+                description: 'A unique objectId',
+                example: '66acfdb89cf7a052700758ad',
+              },
+              name: {
+                type: 'string',
+                description: 'Album name.',
+                example: 'Testimony',
+              },
+              releasedYear: {
+                type: 'number',
+                description: "Album's released year.",
+                example: 2015,
+              },
+              createdAt: {
+                type: 'string',
+                description: 'Date & Time this record was created',
+                example: '2024-08-02T15:39:36.220Z',
+              },
+              updatedAt: {
+                type: 'string',
+                description: 'Date & Time this record was updated',
+                example: '2024-08-02T15:39:36.220Z',
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server code',
+  })
   async getAlbums() {
     const albums = await this.albumsService.getAlbums();
     return {
@@ -29,6 +93,30 @@ export class AlbumsController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Fetch Single Album' })
+  @ApiParam({
+    name: 'id',
+    type: 'string',
+    description: 'Valid objectID',
+    example: '66acfdcd9cf7a052700758af',
+    required: true,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Ok',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Not Found',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error',
+  })
   async getAlbumById(@Param('id') id: string) {
     const isValid = mongoose.Types.ObjectId.isValid(id);
     if (!isValid) throw new HttpException('Invalid ID', 404);
@@ -40,12 +128,71 @@ export class AlbumsController {
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create Album' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string',
+          example: 'Single',
+          description: 'Name of the Album.',
+        },
+        releasedYear: {
+          type: 'number',
+          example: 2058,
+          description: 'The year the album was released.',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'OK',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
   @UsePipes(new ValidationPipe())
   createAlbum(@Body() createAlbumDto: CreateAlbumDto) {
     return this.albumsService.createAlbum(createAlbumDto);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update Album' })
+  @ApiParam({
+    name: 'id',
+    type: 'string',
+    description: 'A unique and valid ObjectId',
+    example: '66acfdcd9cf7a052700758af',
+    required: true,
+  })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string',
+          example: 'Single',
+          description: 'Name of the Album.',
+        },
+        releasedYear: {
+          type: 'number',
+          example: 2058,
+          description: 'The year the album was released.',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'OK',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
   @UsePipes(new ValidationPipe())
   async updateAlbum(
     @Param('id') id: string,
@@ -64,6 +211,21 @@ export class AlbumsController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete Album' })
+  @ApiParam({
+    name: 'id',
+    type: 'string',
+    description: 'A unique and valid ObjectId',
+    required: true,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'OK',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
   async deleteAlbum(@Param('id') id: string) {
     const isValid = mongoose.Types.ObjectId.isValid(id);
     if (!isValid) throw new HttpException('Invalid ID', 404);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Tosin Bee API')
+    .setDescription('This is the API Documentation of Tosin Bee Web App')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
