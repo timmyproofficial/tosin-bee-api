0bfdaa9c6154e7cd671548fa6aeeda06db055dbe
timmyproofficial <timmyshow96@gmail.com>
2024-08-03 03:43:26 +0100

    Document Genre Endpoints

--- src/genres/genres.controller.ts
+++ src/genres/genres.controller.ts
@@ -14,12 +14,71 @@ import mongoose from 'mongoose';
 import { GenresService } from './genres.service';
 import { CreateGenreDto } from './dto/Genre.dto';
 import { UpdateGenreDto } from './dto/UpdateGenre.dto';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBody,
+  ApiParam,
+} from '@nestjs/swagger';
 
+@ApiTags('Genres')
 @Controller('genres')
 export class GenresController {
   constructor(private genresService: GenresService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Fetch Genres' })
+  @ApiResponse({
+    status: 200,
+    description: 'Ok',
+    schema: {
+      type: 'object',
+      properties: {
+        count: {
+          type: 'number',
+          description: 'The length of the result',
+          example: 4,
+        },
+        result: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              _id: {
+                type: 'string',
+                description: 'A unique objectId',
+                example: '66acfdb89cf7a052700758ad',
+              },
+              name: {
+                type: 'string',
+                description: "Genre's name.",
+                example: 'RnB',
+              },
+              createdAt: {
+                type: 'string',
+                description: 'Date & Time this record was created',
+                example: '2024-08-02T15:39:36.220Z',
+              },
+              updatedAt: {
+                type: 'string',
+                description: 'Date & Time this record was updated',
+                example: '2024-08-02T15:39:36.220Z',
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server code',
+  })
   async getGenres() {
     const genres = await this.genresService.getGenres();
     return {
@@ -29,6 +88,30 @@ export class GenresController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Fetch Single Genre' })
+  @ApiParam({
+    name: 'id',
+    type: 'string',
+    description: 'Valid objectID',
+    example: '66ac252721c95e1f23ccc210',
+    required: true,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Ok',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Not Found',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error',
+  })
   async getGenreById(@Param('id') id: string) {
     const isValid = mongoose.Types.ObjectId.isValid(id);
     if (!isValid) throw new HttpException('Genre not found', 404);
@@ -40,12 +123,61 @@ export class GenresController {
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create Genre' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string',
+          example: 'RnB',
+          description: 'Name of the Genre.',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'OK',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
   @UsePipes(new ValidationPipe())
   createGenre(@Body() createGenreDto: CreateGenreDto) {
     return this.genresService.createGenre(createGenreDto);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update Genre' })
+  @ApiParam({
+    name: 'id',
+    type: 'string',
+    description: 'A unique and valid ObjectId',
+    example: '66ac252721c95e1f23ccc210',
+    required: true,
+  })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string',
+          example: 'Apala Gospel',
+          description: 'Name of the Genre.',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'OK',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
   @UsePipes(new ValidationPipe())
   async updateGenre(
     @Param('id') id: string,
@@ -64,6 +196,21 @@ export class GenresController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete Genre' })
+  @ApiParam({
+    name: 'id',
+    type: 'string',
+    description: 'A unique and valid ObjectId',
+    required: true,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'OK',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
   async deleteGenre(@Param('id') id: string) {
     const isValid = mongoose.Types.ObjectId.isValid(id);
     if (!isValid) throw new HttpException('Invalid ID', 404);
