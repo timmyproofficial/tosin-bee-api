279281fb1cc6a2a0ef9066e21b02725c0c423f92
timmyproofficial <timmyshow96@gmail.com>
2024-08-03 03:04:06 +0100

    Document event endpoints

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -14,12 +14,92 @@ import mongoose from 'mongoose';
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/Event.dto';
 import { UpdateEventDto } from './dto/UpdateEvent.dto';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Events')
 @Controller('events')
 export class EventsController {
   constructor(private eventsService: EventsService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Fetch Events' })
+  @ApiResponse({
+    status: 200,
+    description: 'Ok',
+    schema: {
+      type: 'object',
+      properties: {
+        count: {
+          type: 'number',
+          description: 'The length of the result',
+          example: 4,
+        },
+        result: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              _id: {
+                type: 'string',
+                description: 'A unique objectId',
+                example: '66acfdb89cf7a052700758ad',
+              },
+              title: {
+                type: 'string',
+                description: 'Title of the event',
+                example: '02 Arena Praise Medley',
+              },
+              venue: {
+                type: 'string',
+                description: 'The venue of the event',
+                example: 'O2 Arena, London',
+              },
+              description: {
+                type: 'string',
+                description: 'Event details',
+                example:
+                  'A 20K capacity concert. Come and experience high praise...',
+              },
+              eventDate: {
+                type: 'string',
+                description: 'Event Date',
+                example: '20th Oct, 2077',
+              },
+              eventTime: {
+                type: 'string',
+                description: 'Event Time',
+                example: '12:00 AM',
+              },
+              createdAt: {
+                type: 'string',
+                description: 'Date & Time this record was created',
+                example: '2024-08-02T15:39:36.220Z',
+              },
+              updatedAt: {
+                type: 'string',
+                description: 'Date & Time this record was updated',
+                example: '2024-08-02T15:39:36.220Z',
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server code',
+  })
   async getEvents() {
     const events = await this.eventsService.getEvents();
     return {
@@ -29,6 +109,30 @@ export class EventsController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Fetch Single Event' })
+  @ApiParam({
+    name: 'id',
+    type: 'string',
+    description: 'Valid objectID',
+    example: '66acfdcd9cf7a052700758af',
+    required: true,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Ok',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Not Found',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error',
+  })
   async getEventById(@Param('id') id: string) {
     const isValid = mongoose.Types.ObjectId.isValid(id);
     if (!isValid) throw new HttpException('Invalid ID', 404);
@@ -40,12 +144,101 @@ export class EventsController {
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create Event' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        title: {
+          type: 'string',
+          description: 'Title of the event',
+          example: '02 Arena Praise Medley',
+        },
+        venue: {
+          type: 'string',
+          description: 'The venue of the event',
+          example: 'O2 Arena, London',
+        },
+        description: {
+          type: 'string',
+          description: 'Event details',
+          example: 'A 20K capacity concert. Come and experience high praise...',
+        },
+        eventDate: {
+          type: 'string',
+          description: 'Event Date',
+          example: '20th Oct, 2077',
+        },
+        eventTime: {
+          type: 'string',
+          description: 'Event Time',
+          example: '12:00 AM',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'OK',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
   @UsePipes(new ValidationPipe())
   createEvent(@Body() createEventDto: CreateEventDto) {
     return this.eventsService.createEvent(createEventDto);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update Event' })
+  @ApiParam({
+    name: 'id',
+    type: 'string',
+    description: 'A unique and valid ObjectId',
+    example: '66acfdcd9cf7a052700758af',
+    required: true,
+  })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        title: {
+          type: 'string',
+          description: 'Title of the event',
+          example: '02 Arena Praise Medley',
+        },
+        venue: {
+          type: 'string',
+          description: 'The venue of the event',
+          example: 'O2 Arena, London',
+        },
+        description: {
+          type: 'string',
+          description: 'Event details',
+          example: 'A 20K capacity concert. Come and experience high praise...',
+        },
+        eventDate: {
+          type: 'string',
+          description: 'Event Date',
+          example: '20th Oct, 2077',
+        },
+        eventTime: {
+          type: 'string',
+          description: 'Event Time',
+          example: '12:00 AM',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'OK',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
   @UsePipes(new ValidationPipe())
   async updateEvent(
     @Param('id') id: string,
@@ -64,6 +257,21 @@ export class EventsController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete Event' })
+  @ApiParam({
+    name: 'id',
+    type: 'string',
+    description: 'A unique and valid ObjectId',
+    required: true,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'OK',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
   async deleteEvent(@Param('id') id: string) {
     const isValid = mongoose.Types.ObjectId.isValid(id);
     if (!isValid) throw new HttpException('Invalid ID', 404);
